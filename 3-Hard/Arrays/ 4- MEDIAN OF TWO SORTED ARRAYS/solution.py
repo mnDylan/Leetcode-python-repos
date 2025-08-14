817a27e08302e8dee4e3ca96811284d189436b3e
Dylan Ng <124280476+mnDylan@users.noreply.github.com>
2025-08-14 16:39:28 +0700

    Update solution.py

--- 3-Hard/Arrays/ 4- MEDIAN OF TWO SORTED ARRAYS/solution.py
+++ 3-Hard/Arrays/ 4- MEDIAN OF TWO SORTED ARRAYS/solution.py
@@ -1,1 +1,16 @@
-
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        merged_array =  nums1 + nums2
+        merged_array.sort()
+        n= len(merged_array)
+        if  n % 2 == 1: 
+            median = merged_array[n//2]
+            return median 
+        else: 
+            median = (merged_array[n//2 -1] + merged_array[n//2]) / 2.0
+            return median
