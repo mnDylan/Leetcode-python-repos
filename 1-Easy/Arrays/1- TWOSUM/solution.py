ee9848331dd7b078c1c99e507664fe0118c5c4e4
Dylan Ng <124280476+mnDylan@users.noreply.github.com>
2025-08-14 16:20:04 +0700

    Update solution.py

--- 1-Easy/Arrays/1- TWOSUM/solution.py
+++ 1-Easy/Arrays/1- TWOSUM/solution.py
@@ -1,1 +1,15 @@
-
+class Solution(object):
+    def twoSum(self, nums, target):
+    # Create a dictionary to store the value and its index
+        num_map = {}
+        
+        # Iterate through the array
+        for i, num in enumerate(nums):
+            complement = target - num
+            
+            # Check if the complement is already in the map
+            if complement in num_map:
+                return [num_map[complement], i]  # Return the indices
+            
+            # Otherwise, add the current number to the map
+            num_map[num] = i
